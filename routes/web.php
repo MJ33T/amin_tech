83430ed0ce1759c8d9a123c34bf707e06ffa9e14
J33T <muhtasim_jeet@hotmail.com>
2021-01-31 23:27:30 +0600

    Create UserController and Creating Session for admin login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,5 @@ Route::get('/', function () {
 });
 
 Route::view('/login', "login");
-    
+
+Route::post("/login", [UserController::class, 'login']);
